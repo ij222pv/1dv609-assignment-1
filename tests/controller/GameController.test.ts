c259d18c417575647eba3e04fdac0cc4a37a6e36
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 22:07:40 +0100

    RED: test that GameModel.rollDice is called after view dispatches "roll" event

--- tests/controller/GameController.test.ts
+++ tests/controller/GameController.test.ts
@@ -4,9 +4,21 @@ import UI from "../../src/view/UI";
 import Dice from "../../src/model/Dice";
 import GameModel from "../../src/model/GameModel";
 
-class MockUI implements UI {
-  public addListener = jest.fn((eventName: string, listener: Function): void => {});
+class FakeUI implements UI {
+  private listeners: { eventName: string; callback: Function }[] = [];
+
+  public addListener = jest.fn((eventName: string, callback: Function): void => {
+    this.listeners.push({ eventName, callback });
+  });
+
   public showDice = jest.fn((dice: Dice): void => {});
+
+  public callRollListeners(): void {
+    const rollListeners = this.listeners.filter(l => l.eventName === "roll");
+    for (const listener of rollListeners) {
+      listener.callback();
+    }
+  }
 }
 
 class MockGameModel implements GameModel {
@@ -16,11 +28,23 @@ class MockGameModel implements GameModel {
 describe("GameController", () => {
   test("should add roll listener", () => {
     const model = new MockGameModel();
-    const view = new MockUI();
+    const view = new FakeUI();
     const controller = new GameController(model, view);
 
     controller.start();
 
     expect(view.addListener).toHaveBeenCalledWith("roll", expect.any(Function));
+  });
+
+  test("should call model.rollDice when roll event is triggered", () => {
+    const model = new MockGameModel();
+    const view = new FakeUI();
+    const controller = new GameController(model, view);
+
+    controller.start();
+    // Simulate the roll event being triggered
+    view.callRollListeners();
+
+    expect(model.rollDice).toHaveBeenCalled();
   });
 });
