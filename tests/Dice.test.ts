6e40cf3f033a25a755043c1267e31adaa5ff57e7
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 15:37:05 +0100

    REFACTOR: Test that random provider is called

--- tests/Dice.test.ts
+++ tests/Dice.test.ts
@@ -3,18 +3,24 @@ import Dice from "../src/Dice";
 import RandomProvider from "../src/RandomProvider";
 
 class FakeRandomProvider implements RandomProvider {
+  private hasBeenCalled = false;
+
   public getRandomIntegerInRange(min: number, max: number): number {
-    expect(min).toBe(1);
-    expect(max).toBe(6);
+    this.hasBeenCalled = true;
     return 3;
+  }
+
+  public verify(): void {
+    expect(this.hasBeenCalled).toBeTruthy();
   }
 }
 
 describe("Dice", () => {
-  test("should call random provider with correct range", () => {
+  test("should call random provider", () => {
     const fakeRandomProvider = new FakeRandomProvider();
     const die = new Dice(fakeRandomProvider);
     die.roll();
+    fakeRandomProvider.verify();
   });
 
   test("should return correct random number", () => {
