66adf087fabf43c4adf7d8fea5c037806e524d26
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 21:50:51 +0100

    GREEN: create GameController

---
+++ src/controller/GameController.ts
@@ -0,0 +1,10 @@
+import GameModel from "../model/GameModel";
+import UI from "../view/UI";
+
+export default class GameController {
+  public constructor(private model: GameModel, private view: UI) {}
+
+  public start() {
+    this.view.addListener("roll", () => {});
+  }
+}
