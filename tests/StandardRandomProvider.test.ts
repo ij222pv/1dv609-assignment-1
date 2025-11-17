26c70ccb0f6eec1211059ad58d9028494409c865
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 15:28:07 +0100

    RED: Test return value from StandardRandomProvider.getRandomIntegerInRange()

---
+++ tests/StandardRandomProvider.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "@jest/globals";
+import StandardRandomProvider from "../src/StandardRandomProvider";
+
+describe("StandardRandomProvider", () => {
+  describe("getRandomIntegerInRange", () => {
+    // Because we cannot predict the return value we will only check that it is a number.
+    // If we tested that the return value is in the range 1-5, that could create a flaky test.
+    test("should return number", () => {
+      const randomProvider = new StandardRandomProvider();
+      const returnValue = randomProvider.getRandomIntegerInRange(1, 5);
+      expect(typeof returnValue).toBe("number");
+    });
+  });
+});
