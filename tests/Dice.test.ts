89caca1d29c97903e8e6ca8b21fe99547af6e823
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 15:17:29 +0100

    RED: Test Dice return value

--- tests/Dice.test.ts
+++ tests/Dice.test.ts
@@ -3,7 +3,7 @@ import Dice from "../src/Dice";
 import RandomProvider from "../src/RandomProvider";
 
 class FakeRandomProvider implements RandomProvider {
-  public getRandomIntegerInRange(min: number, max: number) {
+  public getRandomIntegerInRange(min: number, max: number): number {
     expect(min).toBe(1);
     expect(max).toBe(6);
     return 3;
@@ -16,4 +16,11 @@ describe("Dice", () => {
     const die = new Dice(fakeRandomProvider);
     die.roll();
   });
+
+  test("should return correct random number", () => {
+    const fakeRandomProvider = new FakeRandomProvider();
+    const die = new Dice(fakeRandomProvider);
+    die.roll();
+    expect(die.getValue()).toBe(3);
+  })
 });
