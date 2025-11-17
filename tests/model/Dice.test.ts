55b8519778ed6e61740945f02adae4cb7d320936
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 20:15:58 +0100

    REFACTOR: extract code into beforeEach in Dice tests

--- tests/model/Dice.test.ts
+++ tests/model/Dice.test.ts
@@ -1,29 +1,31 @@
-import { describe, test, expect, jest } from "@jest/globals";
+import { describe, test, expect, jest, beforeEach } from "@jest/globals";
 import Dice from "../../src/model/Dice";
 import RandomProvider from "../../src/model/RandomProvider";
 
-class FakeRandomProvider implements RandomProvider {
+class MockRandomProvider implements RandomProvider {
   public getRandomIntegerInRange = jest.fn((min, max) => 3);
 }
 
 describe("Dice", () => {
+  let mockRandomProvider: MockRandomProvider;
+  let die: Dice;
+
+  beforeEach(() => {
+    mockRandomProvider = new MockRandomProvider();
+    die = new Dice(mockRandomProvider);
+  });
+
   test("should call random provider on roll", () => {
-    const fakeRandomProvider = new FakeRandomProvider();
-    const die = new Dice(fakeRandomProvider);
     die.roll();
-    expect(fakeRandomProvider.getRandomIntegerInRange).toHaveBeenCalled();
+    expect(mockRandomProvider.getRandomIntegerInRange).toHaveBeenCalled();
   });
 
   test("should return number retrieved from injected RandomProvider", () => {
-    const fakeRandomProvider = new FakeRandomProvider();
-    const die = new Dice(fakeRandomProvider);
     die.roll();
     expect(die.getValue()).toBe(3);
   });
 
   test("should have a value before rolling", () => {
-    const fakeRandomProvider = new FakeRandomProvider();
-    const die = new Dice(fakeRandomProvider);
     expect(die.getValue()).toBeDefined();
   });
 });
