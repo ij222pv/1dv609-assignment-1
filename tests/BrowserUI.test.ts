5a8cddd74d42f8710f6e1b310315095492efb122
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-17 17:12:25 +0100

    RED: Add test checking dice image src

--- tests/BrowserUI.test.ts
+++ tests/BrowserUI.test.ts
@@ -25,4 +25,13 @@ describe("BrowserUI", () => {
 
     expect(gameDiv.querySelectorAll("img[class='dice']").length).toBe(1);
   });
+
+  test('should show correct image for dice value', () => {
+    const gameDiv = document.createElement("div");
+    const ui = new BrowserUI(gameDiv);
+
+    ui.showDice(4);
+
+    expect(gameDiv.querySelector(`img[src='images/dice4.png']`)).not.toBeNull();
+  });
 });
