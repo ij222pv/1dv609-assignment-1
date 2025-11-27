631ad60de1ad1038a08fd537eb5c74e10c17ae3f
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-27 19:34:17 +0100

    GREEN: throw error on empty name

--- src/model/Player.ts
+++ src/model/Player.ts
@@ -1,5 +1,9 @@
 export default class Player {
-  public constructor(private name: string) {}
+  public constructor(private name: string) {
+    if (name === "") {
+      throw new Error("Player name cannot be empty");
+    }
+  }
 
   public getName(): string {
     return this.name;
