45ab7b518495e9c462275cc3e96d6abbff9c8a34
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-27 19:26:35 +0100

    RED: add test for checking Player name

---
+++ tests/model/Player.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "@jest/globals";
+import Player from "../../src/model/Player";
+
+describe("Player", () => {
+  test("should store name", () => {
+    const name = "Alice";
+
+    const sut = new Player(name);
+
+    expect(sut.getName()).toBe(name);
+  });
+});
