ba0f65a5035918d30ebd42a6d517c2b4f5e7f1ef
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-27 19:24:20 +0100

    GREEN: add methods for interacting with players and add Player class

--- src/model/GameModelImpl.ts
+++ src/model/GameModelImpl.ts
@@ -1,13 +1,24 @@
-import Dice from "./Dice";
-import DiceFactory from "./DiceFactory";
-import GameModel from "./GameModel";
+import type Dice from "./Dice";
+import type DiceFactory from "./DiceFactory";
+import type GameModel from "./GameModel";
+import Player from "./Player";
 
 export default class GameModelImpl implements GameModel {
+  private players: Player[] = [];
+
   public constructor(private diceFactory: DiceFactory) {}
 
   public rollDice(): Dice {
     const dice = this.diceFactory.createDice();
     dice.roll();
     return dice;
+  }
+
+  public addPlayer(player: Player): void {
+    this.players.push(player);
+  }
+
+  public getPlayers(): Player[] {
+    return this.players;
   }
 }
