3a3a1312cd8e0280ae1fd71746c59d8b8946ba63
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-27 19:15:47 +0100

    RED: add test for adding a player to the game

--- tests/model/GameModelImpl.test.ts
+++ tests/model/GameModelImpl.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import GameModelImpl from "../../src/model/GameModelImpl";
 import D6DiceFactory from "../../src/model/D6DiceFactory";
+import Player from "../../src/model/Player";
 
 describe("GameModelImpl", () => {
   test("should return dice with value 3", () => {
@@ -10,5 +11,16 @@ describe("GameModelImpl", () => {
 
     const dice = gameModel.rollDice();
     expect(dice.getValue()).toBe(3);
+  });
+
+  test("should add a player to the game", () => {
+    const gameModel = new GameModelImpl(new D6DiceFactory({
+      getRandomIntegerInRange: (min: number, max: number): number => 1,
+    }));
+
+    const player = new Player("Alice");
+    gameModel.addPlayer(player);
+
+    expect(gameModel.getPlayers()[0]).toBe(player);
   });
 });
