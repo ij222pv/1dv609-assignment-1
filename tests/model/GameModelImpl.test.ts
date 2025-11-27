b99477f52c810755d90882923f6bcd5e326d2d20
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-27 19:31:48 +0100

    REFACTOR: extract duplicated code

--- tests/model/GameModelImpl.test.ts
+++ tests/model/GameModelImpl.test.ts
@@ -1,23 +1,24 @@
-import { describe, expect, test } from "@jest/globals";
+import { beforeEach, describe, expect, test } from "@jest/globals";
 import GameModelImpl from "../../src/model/GameModelImpl";
 import D6DiceFactory from "../../src/model/D6DiceFactory";
 import Player from "../../src/model/Player";
+import type GameModel from "../../src/model/GameModel";
 
 describe("GameModelImpl", () => {
-  test("should return dice with value 3", () => {
-    const gameModel = new GameModelImpl(new D6DiceFactory({
+  let gameModel: GameModel;
+
+  beforeEach(() => {
+    gameModel = new GameModelImpl(new D6DiceFactory({
       getRandomIntegerInRange: (min: number, max: number): number => 3,
     }));
+  });
 
+  test("should return dice with value 3", () => {
     const dice = gameModel.rollDice();
     expect(dice.getValue()).toBe(3);
   });
 
   test("should add a player to the game", () => {
-    const gameModel = new GameModelImpl(new D6DiceFactory({
-      getRandomIntegerInRange: (min: number, max: number): number => 1,
-    }));
-
     const player = new Player("Alice");
     gameModel.addPlayer(player);
 
