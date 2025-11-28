13c966ac9cc84b10353178fee4688aa171d3a654
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-11-28 18:24:33 +0100

    REFACTOR: restructure tests and remove duplicate code

--- tests/model/GameModelImpl.test.ts
+++ tests/model/GameModelImpl.test.ts
@@ -57,86 +57,88 @@ describe("GameModelImpl", () => {
         lastActivePlayer = currentActivePlayer;
       }
     });
-  });
 
-  test("should throw error when there are no players", () => {
-    expect(() => {
-      gameModel.getActivePlayer();
-    }).toThrow();
-  });
+    test("should throw error when there are no players", () => {
+      gameModel = createMockedGameModel([1], []);
 
-  test("should not change active player when there is only one player", () => {
-    gameModel = createMockedGameModel([1], [new Player("Solo")]);
-    const activePlayerBefore = gameModel.getActivePlayer();
+      expect(() => {
+        gameModel.getActivePlayer();
+      }).toThrow();
+    });
 
-    // Roll a 1
-    gameModel.rollDice();
+    test("should not change active player when there is only one player", () => {
+      gameModel = createMockedGameModel([1], createPlayerArray(1));
+      const activePlayerBefore = gameModel.getActivePlayer();
 
-    expect(gameModel.getActivePlayer()).toBe(activePlayerBefore);
-  });
-
-  test("rolling any number other than 1 should not change the active player", () => {
-    gameModel = createMockedGameModel([2, 3, 4, 5, 6], [new Player("Player1"), new Player("Player2")]);
-    const activePlayerBefore = gameModel.getActivePlayer();
-
-    for (let i = 0; i < 5; i++) {
+      // Roll a 1
       gameModel.rollDice();
+
       expect(gameModel.getActivePlayer()).toBe(activePlayerBefore);
-    }
-  });
+    });
 
-  test("should change active player when a 1 is rolled and there are three players", () => {
-    gameModel = createMockedGameModel([1], [new Player("Player1"), new Player("Player2"), new Player("Player3")]);
-    const listOfActivePlayerEachRound: Player[] = [];
+    test("rolling any number other than 1 should not change the active player", () => {
+      gameModel = createMockedGameModel([2, 3, 4, 5, 6], createPlayerArray(2));
+      const activePlayerBefore = gameModel.getActivePlayer();
 
-    for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < 5; i++) {
+        gameModel.rollDice();
+        expect(gameModel.getActivePlayer()).toBe(activePlayerBefore);
+      }
+    });
+
+    test("should change active player when a 1 is rolled and there are three players", () => {
+      gameModel = createMockedGameModel([1], createPlayerArray(3));
+      const listOfActivePlayerEachRound: Player[] = [];
+
+      for (let i = 0; i < 3; i++) {
+        gameModel.rollDice();
+        listOfActivePlayerEachRound.push(gameModel.getActivePlayer());
+      }
+
+      // We expect that all three players have been active at least once after three rounds.
+      expect(new Set(listOfActivePlayerEachRound).size).toBe(3);
+    });
+
+    test("should go to next player when ending turn", () => {
+      gameModel = createMockedGameModel([2], createPlayerArray(2));
+      const activePlayerBefore = gameModel.getActivePlayer();
+
       gameModel.rollDice();
-      listOfActivePlayerEachRound.push(gameModel.getActivePlayer());
-    }
+      gameModel.endTurn();
 
-    // We expect that all three players have been active at least once after three rounds.
-    expect(new Set(listOfActivePlayerEachRound).size).toBe(3);
+      expect(gameModel.getActivePlayer()).not.toBe(activePlayerBefore);
+    });
   });
 
-  test("should add the sum of the dice rolls this turn to the player's score", () => {
-    gameModel = createMockedGameModel([2, 4], [new Player("Player1"), new Player("Player2")]);
-    const activePlayerBefore = gameModel.getActivePlayer();
+  describe("score calculation", () => {
+    test("should add the sum of the dice rolls this turn to the player's score", () => {
+      gameModel = createMockedGameModel([2, 4], createPlayerArray(2));
+      const activePlayerBefore = gameModel.getActivePlayer();
 
-    gameModel.rollDice();
-    gameModel.rollDice();
-    gameModel.endTurn();
+      gameModel.rollDice();
+      gameModel.rollDice();
+      gameModel.endTurn();
 
-    expect(activePlayerBefore.getScore()).toBe(6);
-  });
+      expect(activePlayerBefore.getScore()).toBe(6);
+    });
 
-  test("rolling a 1, then rolling a 3, then ending turn should add only 3 to the player's score", () => {
-    const player1 = new Player("Player1");
-    const player2 = new Player("Player2");
-    gameModel = createMockedGameModel([1, 3], [player1, player2]);
+    test("rolling a 1, then rolling a 3, then ending turn should add only 3 to the player's score", () => {
+      const player1 = new Player("Player1");
+      const player2 = new Player("Player2");
+      gameModel = createMockedGameModel([1, 3], [player1, player2]);
 
-    // Roll 1. This ends Player1's turn.
-    gameModel.rollDice();
-    // Roll 3 for Player2.
-    gameModel.rollDice();
-    gameModel.endTurn();
+      // Roll 1. This ends Player1's turn.
+      gameModel.rollDice();
+      // Roll 3 for Player2.
+      gameModel.rollDice();
+      gameModel.endTurn();
 
-    expect(player2.getScore()).toBe(3);
-  });
-
-  test("should go to next player when ending turn", () => {
-    gameModel = createMockedGameModel([2], [new Player("Player1"), new Player("Player2")]);
-    const activePlayerBefore = gameModel.getActivePlayer();
-
-    gameModel.rollDice();
-    gameModel.endTurn();
-
-    expect(gameModel.getActivePlayer()).not.toBe(activePlayerBefore);
+      expect(player2.getScore()).toBe(3);
+    });
   });
 
   test("should call activePlayerChange event when changing active player", () => {
-    const player1 = new Player("Player1");
-    const player2 = new Player("Player2");
-    gameModel = createMockedGameModel([1], [player1, player2]);
+    gameModel = createMockedGameModel([1], createPlayerArray(2));
     const mockListener = jest.fn();
 
     gameModel.addListener("activePlayerChange", mockListener);
@@ -161,4 +163,12 @@ function createMockedGameModel(diceResults: number[], players?: Player[]): GameM
   );
 
   return game;
+}
+
+function createPlayerArray(howMany: number): Player[] {
+  const players: Player[] = [];
+  for (let i = 0; i < howMany; i++) {
+    players.push(new Player(`Player ${i + 1}`));
+  }
+  return players;
 }
