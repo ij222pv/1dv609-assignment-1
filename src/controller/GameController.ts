a4704e51a53192fc4043473e737845509f8c8230
Isak Johansson Weckstén <ij222pv@student.lnu.se>
2025-12-04 13:07:02 +0100

    GREEN: call model.addPlayer on addPlayer event

--- src/controller/GameController.ts
+++ src/controller/GameController.ts
@@ -1,5 +1,6 @@
 import Dice from "../model/Dice";
 import GameModel from "../model/GameModel";
+import Player from "../model/Player";
 import UI from "../view/UI";
 
 export default class GameController {
@@ -7,7 +8,7 @@ export default class GameController {
 
   public start() {
     this.view.addSubscriber("roll", () => {
-      const dice = this.model.rollDice.bind(this.model)();
+      this.model.rollDice.bind(this.model)();
     });
 
     this.model.addSubscriber("activePlayerChange", () => {
@@ -20,6 +21,10 @@ export default class GameController {
 
     this.model.addSubscriber("diceRolled", (dice: Dice) => {
       this.view.showDice(dice);
+    });
+
+    this.view.addSubscriber("addPlayer", (name: string) => {
+      this.model.addPlayer(new Player(name));
     });
   }
 }
